3d509758e8bbdaaff08a31089503a8e055c6d94e
NickSilva121 <pereira.nicolas1210@gmail.com>
2025-12-01 21:49:27 -0300

    upload

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+livros = [
+    {
+        'id': 1,
+        'titulo': 'As Crônicas de Nárnia - O Leão, a Feiticeira e o Guarda-Roupa',
+        'autor': 'C.S. Lewis'
+    },
+    {
+        'id': 2,
+        'titulo': 'Harry Potter e a Pedra Filosofal',
+        'autor': 'J.K Howling'
+    },
+    {
+        'id': 3,
+        'titulo': 'Os 7 Hábitos das Pessoas Altamente Eficazes',
+        'autor': 'Sean Covey'
+    },
+]
+
+# Consulta (todos)
+@app.route('/livros', methods=['GET'])
+def get_livros():
+    return jsonify(livros)
+# Consulta (id)
+@app.route('/livros/<int:id>',  methods=['GET'])
+def get_livro_id(id):
+    for livro in livros:
+        if livro.get('id') == id:
+            return jsonify(livro)
+# Edição
+@app.route('/livros/<int:id>', methods=['PUT'])
+def edit_livro_id(id):
+    livro_alt = request.get_json()
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            livros[indice].update(livro_alt)
+            return jsonify(livros[indice])
+# Criar
+@app.route('/livros', methods=['POST'])
+def create_livro():
+    new_livro = request.get_json()
+    livros.append(new_livro)
+
+    return jsonify(livros)
+# Exclusão
+@app.route('/livros/<int:id>', methods=['DELETE'])
+def exclui_livro(id):
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            del livros[indice]
+    return jsonify(livros)
+app.run(port=5000, host='localhost', debug=True)
---
+++ readme.md
@@ -0,0 +1,10 @@
+# API é um lugar para disponibilizar recursos e/ou funcionalidades
+# 1. Onjetivo - Criar uma API que disponibiliza a consulta, criação, edição e exclusão de livros
+# 2. URL base - localhost
+# 3. Endpoints - 
+#   - localhost/livros [GET]
+#   - localhost/livros [POST]
+#   - localhost/livros/id [GET]
+#   - localhost/livro/id [PUT]
+#   - localhost/livro [DELETE]
+# 4. Quais recursos- Livros
